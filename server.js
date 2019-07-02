7a7ef4a09bd4d464bbd7f236fbf527d5f12ae31b
Grzegorz Olszewski <gino17m@gmail.com>
2019-07-02 23:15:18 +0200

    gggg

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/client/dist'));
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, '/client/dist', 'index.html'));
+});
 app.listen(process.env.PORT || 8080);
