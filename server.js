ba3ee09d1d1accd3b4b5f2fdf6a695eec1c8c5d4
Grzegorz Olszewski <gino17m@gmail.com>
2019-07-03 11:50:29 +0200

    ssssa

--- server.js
+++ server.js
@@ -2,7 +2,5 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('client/build'));
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+
 app.listen(process.env.PORT || 8080);
